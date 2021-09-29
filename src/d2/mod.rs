f8591b0872355a689c68c933371338cb73facf0d
hatoo <hato2000@gmail.com>
2021-09-30 00:01:17 +0900

    advect

--- src/d2/mod.rs
+++ src/d2/mod.rs
@@ -1,13 +1,14 @@
-use ndarray::Array2;
+use cgmath::{vec2, Vector2};
+use ndarray::{Array, Array2};
 
-pub fn interpolate_linear(q: &Array2<f64>, (i, j): (f64, f64)) -> f64 {
+fn interpolate_linear(q: &Array2<Vector2<f64>>, ij: Vector2<f64>) -> Vector2<f64> {
     let (w, h) = q.dim();
 
     assert!(w >= 1);
     assert!(h >= 1);
 
-    let x = i.max(0.0).min((w - 1) as f64);
-    let y = j.max(0.0).min((h - 1) as f64);
+    let x = ij.x.max(0.0).min((w - 1) as f64);
+    let y = ij.y.max(0.0).min((h - 1) as f64);
 
     let i0 = x as usize;
     let i1 = (i0 + 1).min(w - 1);
@@ -24,14 +25,14 @@ pub fn interpolate_linear(q: &Array2<f64>, (i, j): (f64, f64)) -> f64 {
     (q[[i0, j0]] * t0 + q[[i0, j1]] * t1) * s0 + (q[[i1, j0]] * t0 + q[[i1, j1]] * t1) * s1
 }
 
-pub fn interpolate_bicubic(q: &Array2<f64>, (i, j): (f64, f64)) -> f64 {
+fn interpolate_bicubic(q: &Array2<f64>, ij: Vector2<f64>) -> f64 {
     let (w, h) = q.dim();
 
     assert!(w >= 4);
     assert!(h >= 4);
 
-    let x = i.max(1.0).min((w - 3) as f64);
-    let y = j.max(1.0).min((h - 3) as f64);
+    let x = ij.x.max(1.0).min((w - 3) as f64);
+    let y = ij.y.max(1.0).min((h - 3) as f64);
 
     let i = x as usize;
     let j = y as usize;
@@ -69,19 +70,39 @@ pub fn interpolate_bicubic(q: &Array2<f64>, (i, j): (f64, f64)) -> f64 {
     q_m1 * t_m1 + q_0 * t_0 + q_p1 * t_p1 + q_p2 * t_p2
 }
 
+pub fn advect(d0: &Array2<f64>, uv: &Array2<Vector2<f64>>, coeff: f64) -> Array2<f64> {
+    assert_eq!(d0.dim(), uv.dim());
+
+    Array::from_shape_fn(d0.dim(), |(i, j)| {
+        let x0 = vec2(i as f64, j as f64);
+        let k1 = uv[[i, j]];
+        let k2 = interpolate_linear(uv, x0 - 0.5 * coeff * k1);
+        let k3 = interpolate_linear(uv, x0 - 0.75 * coeff * k2);
+
+        let v = x0 - 2.0 / 9.0 * coeff * k1 - 3.0 / 9.0 * coeff * k2 - 4.0 / 9.0 * coeff * k3;
+
+        interpolate_bicubic(d0, v)
+    })
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     use approx::assert_abs_diff_eq;
     use ndarray::array;
 
+    fn close_v2(v1: Vector2<f64>, v2: Vector2<f64>) {
+        assert_abs_diff_eq!(v1.x, v2.x);
+        assert_abs_diff_eq!(v1.y, v2.y);
+    }
+
     #[test]
     fn test_interpolate_linear() {
-        let q = array![[0.0, 1.0, 2.0]];
+        let q = array![[vec2(0.0, 0.0), vec2(1.0, 1.0), vec2(2.0, 2.0)]];
 
-        assert_abs_diff_eq!(interpolate_linear(&q, (0.0, 0.0)), 0.0);
-        assert_abs_diff_eq!(interpolate_linear(&q, (0.0, 0.5)), 0.5);
-        assert_abs_diff_eq!(interpolate_linear(&q, (0.0, 1.0)), 1.0);
+        close_v2(interpolate_linear(&q, vec2(0.0, 0.0)), vec2(0.0, 0.0));
+        close_v2(interpolate_linear(&q, vec2(0.0, 0.5)), vec2(0.5, 0.5));
+        close_v2(interpolate_linear(&q, vec2(0.0, 1.0)), vec2(1.0, 1.0));
     }
 
     #[test]
@@ -93,14 +114,14 @@ mod test {
             [0.0, 0.0, 1.0, 1.0, 0.0],
         ];
 
-        assert_abs_diff_eq!(interpolate_bicubic(&q, (1.0, 1.0)), 0.0);
-        assert_abs_diff_eq!(interpolate_bicubic(&q, (1.0, 1.5)), 0.5);
-        assert_abs_diff_eq!(interpolate_bicubic(&q, (1.0, 2.0)), 1.0);
+        assert_abs_diff_eq!(interpolate_bicubic(&q, vec2(1.0, 1.0)), 0.0);
+        assert_abs_diff_eq!(interpolate_bicubic(&q, vec2(1.0, 1.5)), 0.5);
+        assert_abs_diff_eq!(interpolate_bicubic(&q, vec2(1.0, 2.0)), 1.0);
 
         for i in 0..4 {
             for j in 0..4 {
                 // Ensure no index out of bounds
-                interpolate_bicubic(&q, (i as f64, j as f64));
+                interpolate_bicubic(&q, vec2(i as f64, j as f64));
             }
         }
     }
